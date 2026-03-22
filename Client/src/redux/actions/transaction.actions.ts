97f1e7de389d243c768ee813c3f42ee38e6f6d20
unknown <antoinecarrette@hotmail.fr>
2026-03-22 09:50:31 +0100

    build: transaction action 2 22/03/2026

--- Client/src/redux/actions/transaction.actions.ts
+++ Client/src/redux/actions/transaction.actions.ts
@@ -1,6 +1,7 @@
 import axios from "axios"
 
 export type TransactionType = "income" | "expense";
+export type RecurrenceType = "monthly" | "yearly" | null
 
 export interface Transaction {
   _id: string;
@@ -9,14 +10,17 @@ export interface Transaction {
   amount: number;
   date: string;
   recurring: boolean;
-  recurrenceType: "monthly" | "yearly" | null;
+  recurrenceType: RecurrenceType
   description?: string;
 }
 
 export const GET_TRANSACTION = "GET_TRANSACTION"
 export const GET_ALL_TRANSACTION = "GET_ALL_TRANSACTION"
+export const ADD_TRANSACTION = "ADD_TRANSACTION"
+export const GET_TRANSACTION_ERROR = "GET_TRANSACTION_ERROR"
+export const UPDATE_TRANSACTION = "UPDATE_TRANSACTION"
 
-export const getTransaction = async (num: number, dispatch: any): Promise<Transaction[]> => {
+export const getTransaction = async (num: number, dispatch: any) => {
   return axios
     .get(`${process.env.REACT_APP_API_URL}api/transaction`)
     .then((res:any)=> {
@@ -26,25 +30,43 @@ export const getTransaction = async (num: number, dispatch: any): Promise<Transa
     .catch((err:any) => window.alert(err))
 }
 
-export const createTransactionAPI = async (
-  data: Partial<Transaction>
-): Promise<Transaction> => {
-  const res = await axios.post(API_URL, data, {
-    withCredentials: true,
-  });
-  return res.data;
-};
+export const addTransaction = (data: any, dispatch:any) => {
+    return axios 
+        .post(`${process.env.REACT_APP_API_URL}api/snpashot`, data)
+        .then((res:any)=> {
+            if (res.data.errors) {
+                dispatch({type: GET_TRANSACTION_ERROR, payload: res.data.errors})
+            } else {
+                dispatch ({type: GET_TRANSACTION_ERROR, payload:""})
+                dispatch ({type: ADD_TRANSACTION})
+            }
+        })
+}
 
-// UPDATE
-export const updateTransactionAPI = async (
-  id: string,
-  data: Partial<Transaction>
-): Promise<Transaction> => {
-  const res = await axios.put(`${API_URL}/${id}`, data, {
-    withCredentials: true,
-  });
-  return res.data;
-};
+export const updateArticle = (
+  transactionId: string,  
+  type: TransactionType,
+  category: string,
+  amount: number,
+  date: Date,
+  recurring: boolean,
+  recurrenceType: RecurrenceType,
+  description?: string,
+  dispatch:any
+) => {
+        return axios({
+            method:"put",
+            url:`${process.env.REACT_APP_API_URL}api/article/${transactionId}`,
+            data: { type, category, amount, date, recurring, recurrenceType, description},
+        })
+        .then(()=> {
+            dispatch({
+                type: UPDATE_TRANSACTION,
+                payload: {transactionId, type, category, amount, date, recurring, recurrenceType, description}
+            })
+        })
+        .catch((err:any)=> window.alert(err))
+}
 
 // DELETE
 export const deleteTransactionAPI = async (
