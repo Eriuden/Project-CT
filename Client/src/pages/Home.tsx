69cda387d3ad2e2a34cc2fe72b09faceeb7e4bc0
unknown <antoinecarrette@hotmail.fr>
2026-05-05 09:12:56 +0200

    fix: home page 3 retire snapshot 05/04/2026

--- Client/src/pages/Home.tsx
+++ Client/src/pages/Home.tsx
@@ -4,17 +4,29 @@ import { UidContext } from "../components/appContext"
 import { useContext } from 'react'
 import { isEmpty } from '../components/Utils'
 import { Connexion } from "../components/ConnexionModal"
+import { getAllSnapshots } from "../redux/actions/monthlySnapshot.actions"
+import { getTransaction } from "../redux/actions/transaction.actions"
+
+const transactionProps = {
+
+}
 
 export const Home = () => {
 
-    type appDispatch = () => any 
-    const uid = useContext(UidContext)
+    type appDispatch = (transaction) => any 
     const useAppDispatch = () => useDispatch<appDispatch>()
     const dispatch = useAppDispatch()
     setTimeout(() => {
       const preloader = document.querySelector(".preloader") as HTMLElement
       preloader.style.display = "none";
     }, 10000);
+
+    const uid = useContext(UidContext)
+    const transaction = useSelector((state:any) => state.transactionReducer)
+
+    useEffect(()=> {
+        getTransaction(transaction.transactionId, dispatch)
+    })
 
     return (
         <div>
