9a2fa212405060ae987d573e5f2bd2560127769c
unknown <antoinecarrette@hotmail.fr>
2026-05-05 09:04:51 +0200

    fix: app context 05/04/2026

--- Client/src/pages/Home.tsx
+++ Client/src/pages/Home.tsx
@@ -1,20 +1,24 @@
 import { useState, useEffect } from "react"
 import { useDispatch, useSelector } from 'react-redux'
-import { UidContext } from '../components/AppContext'
+import { UidContext } from "../components/appContext"
 import { useContext } from 'react'
 import { isEmpty } from '../components/Utils'
+import { Connexion } from "../components/ConnexionModal"
 
 export const Home = () => {
     type appDispatch = () => any 
-
+    const uid = useContext(UidContext)
     return (
         <div>
             {uid ? (
                 <>
                     <div className="preloader">
-                        
+                      <h2>Veuillez patienter</h2>
+                      <span className="loader"></span>
                     </div>
                 </>
+            ) : (
+                <Connexion/>
             )}
         </div>
     )
