5aca1b46abd284adc054307f3ff22dc40242c6ba
unknown <antoinecarrette@hotmail.fr>
2026-05-12 15:13:35 +0200

    build: App 1 12/05/2026

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,10 +1,43 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import { Routes, Route } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import { Home } from './pages/Home'
+import { getUser } from './redux/actions/user.actions'
+import { ResetPassword } from './pages/ResetPassword'
+import UpdatePassword from './pages/UpdatePassword'
+
+import './App.css'
 
 function App() {
+  type appDispatch = () => any
+  
+  const [uid, setUid] = useState("")
+  const useAppDispatch = () => useDispatch<appDispatch>()
+  const dispatch = useAppDispatch()
+
+  useEffect(()=> {
+    const fetchToken = async () => {
+      await axios({
+        method:"get",
+        url: `${process.env.REACT_APP_API_URL}/jwtid`,
+        withCredentials: true
+      })
+      .then((res:any) => {
+        console.log(res)
+        setUid(res.data)
+      })
+      .catch(() => console.log("Pas de tokens"))
+    }
+    fetchToken()
+    if (uid)
+     getUser(uid, dispatch)
+  }, [uid])
 
   return (
-    <>
-      
-    </>
+    <div>
+
+    </div>
   )
 }
 
